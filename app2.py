7e13fc0bf5443998c680b1aca7afc15c76cfc78a
Larguech15 <mahdilarguech2019@gmail.com>
2026-04-22 02:37:47 +0400

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session 
 from werkzeug.utils import secure_filename
 import os
 import json
